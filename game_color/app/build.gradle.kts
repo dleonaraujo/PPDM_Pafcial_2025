ea4ea6c8411c7a545d1970d1c2207d8c61b90f87
dleonaraujo <dleonaraujo@ulasalle.edu.pe>
2025-05-10 11:42:55 -0500

    Subiendo nav_graph

--- game_color/app/build.gradle.kts
+++ game_color/app/build.gradle.kts
@@ -50,7 +50,6 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
