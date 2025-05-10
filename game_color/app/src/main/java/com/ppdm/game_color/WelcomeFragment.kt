729adc0e69afd5a97e1480fe5c4021c265c8ccd0
dleonaraujo <dleonaraujo@ulasalle.edu.pe>
2025-05-10 11:49:23 -0500

    Creando la funcion WelcomeFragment

---
+++ game_color/app/src/main/java/com/ppdm/game_color/WelcomeFragment.kt
@@ -0,0 +1,20 @@
+package com.ppdm.game_color
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+class WelcomeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        val startButton = view.findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener {
+            findNavController().navigate(R.id.action_welcome_to_game)
+        }
+        return view
+    }
+}
